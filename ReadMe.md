654ccc816f082daeb5b1ac65383b7bf29ef6e51c
Sebastian <sloik89@gmail.com>
2025-04-06 16:12:24 +0200

    integreting mongodb

--- ReadMe.md
+++ ReadMe.md
@@ -3,3 +3,11 @@ Install Packages and Setup Install Script
 ```sh
 npm install bcryptjs@2.4.3 concurrently@8.0.1 cookie-parser@1.4.6 dayjs@1.11.7 dotenv@16.0.3 express@4.18.2 express-async-errors@3.1.1 express-validator@7.0.1 http-status-codes@2.2.0 jsonwebtoken@9.0.0 mongoose@7.0.5 morgan@1.10.0 multer@1.4.5-lts.1 nanoid@4.0.2 nodemon@2.0.22 cloudinary@1.37.3 dayjs@1.11.9 datauri@4.1.0 helmet@7.0.0 express-rate-limit@6.8.0 express-mongo-sanitize@2.2.0
 ```
+
+### Not found and Error Middleware
+
+The 'not found' middleware in Express.js is used when a request is made to a route that does not exist. It catches these request nad responds with a 404 status code, indicating that the requested resource was not found.
+
+On the other hand, the "error" middleware in Express.js is used to handle any errors that occur during the processing of a request. It is typicaly used to catch unexpected errors or exceptions that are not explicityly handled in the application code. It logs the error and sends a 500 status code, indicating an internal server error.
+
+In summary, the "not found" middleware is specifically designed to handle request for non-existent routes, while the "error" middleware is catch-all for handling unexpected errors that occur durning request processing.
--- server.js
+++ server.js
@@ -1,68 +1,40 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 import morgan from 'morgan';
 import { nanoid } from 'nanoid';
+import { createJob, deleteJob, editJob, getAllJobs, getSingleJob } from './controllers/jobsControllers.js';
 dotenv.config()
 
-let jobs = [
-    {id:nanoid(),company:'apple', position:'front-end'},
-    {id:nanoid(),company:'google', position:'back-end'},
-]
+
 const app = express()
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
-
+// routers
+import jobsRouter from './routes/jobRoute.js';
 app.use(express.json())
 
-app.get('/',(req,res) =>
-   res.json({msg:'app working',data:req.body})
-)
-app.get('/api/v1/jobs',(req,res) => {
-    res.json(jobs)
+
+
+
+app.use('/api/v1/jobs',jobsRouter)
+app.use('*',(req,res) => {
+    res.status(404).json({msg:'not found'})
 })
-app.post('/api/v1/jobs',(req,res) => {
-    const {company, position} = req.body
-    if(!company || !position){
-        return res.status(400).json({msg:'please provide company and postion'})
-    }
-    const item = {id:nanoid(10),company,position}
-    jobs.push(item)
-    res.status(200).json({item})
+app.use((err,req,res,next) => {
+    console.log(err)
+    res.status(500).json({msg:'something went wrong'})
 })
-
-app.get('/api/v1/jobs/:id',(req,res) => {
-    const {id} = req.params
-    const job = jobs.find(item=>item.id === id)
-    if(!job){
-        return res.status(404).json({msg:`no job with id ${id}`})
-    }
-    res.status(200).json({job})
-})
-
-
-app.patch('/api/v1/jobs/:id',(req,res) => {
-    const {position,company} = req.body
-   
-    if(!company || !position){
-        return res.status(400).json({msg:'please provide company and postion'})
-    }
-    const {id} = req.params
-    const job = jobs.find(item=>item.id === id)
-    if(!job){
-        return res.status(404).json({msg:`no job with id ${id}`})
-    }
-    job.company = company
-    job.position = position
-
-    res.status(200).json({msg:'job modified',job})
-})
-
-app.post('/',(req,res) =>
-    res.json({msg:'app working',data:req.body})
- )
-
  const port = process.env.PORT || 5001
-app.listen(port,() => {
+
+try{
+ await mongoose.connect(process.env.MONGO_URL)
+ app.listen(port,() => {
     console.log(`server lisening on port ${port}`)
 })
+}
+catch(error){
+    console.log(error)
+    process.exit(1)
+}
